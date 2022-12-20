efe37375a928b7e213f578c42eb9be4144868f13
khumalo611 <s224580434@mandela.ac.za>
2022-12-20 15:05:42 +0200

    Add project files.

---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using eTickets.Data.Enums;
+using System;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
