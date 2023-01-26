63e1d1c8395a0446a5ceb03d59c4f6cda3602b52
khumalo611 <s224580434@mandela.ac.za>
2023-01-26 09:39:29 +0200

    added search, order and order item

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -29,5 +29,9 @@ namespace eTickets.Data
         public DbSet<Actor_Movie> Actor_Movies { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Producer> Producers { get; set; }
-    }
+
+		//Orders related tables
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<OrderItem> OrderItems { get; set; }
+	}
 }
